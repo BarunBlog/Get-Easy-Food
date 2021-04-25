ba3ca90a0e30547a2eb890ce84c3db37267619f8
Barun <bhattacharjeebarun25@gmail.com>
2021-04-25 20:22:17 +0600

    Menu section added

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ $(document).ready(function(){
         $('.navbar').toggleClass('nav-toggle');
     });
     
+    $(window).on('scroll load', function(){
+        $('#menu-bar').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+    });
+
+    $('.menu .list .btn').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+
+        let src = $(this).attr('data-src');
+        $('#menu-img').attr('src', src);
+    })
 });
